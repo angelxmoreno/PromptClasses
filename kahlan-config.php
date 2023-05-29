caa1dd2c9b3e8c0f6735f4de56644f72d01c723d
Angel S. Moreno <angelxmoreno@gmail.com>
2023-05-29 16:55:13 -0400

    created config files

---
+++ kahlan-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var Kahlan\Cli\Kahlan $this
+ */
+use Kahlan\Filter\Filters;
+use Kahlan\Reporter\Coverage\Exporter\Coveralls;
+
+
+$commandLine = $this->commandLine();
+$commandLine->option('src', 'default','src');
+$commandLine->option('spec', 'default', 'tests');
+$commandLine->option('ff', 'default', 1);
+$commandLine->option('coverage', 'default', 3);
+//$commandLine->option('coverage-scrutinizer', 'default', 'scrutinizer.xml');
+//$commandLine->option('coverage-coveralls', 'default', 'coveralls.json');

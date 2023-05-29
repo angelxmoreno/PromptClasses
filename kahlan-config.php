8b2df2761a2f15bdfb6ddeca56c37df201883dfe
Angel S. Moreno <angelxmoreno@gmail.com>
2023-05-29 17:26:03 -0400

    created passing tests

--- kahlan-config.php
+++ kahlan-config.php
@@ -9,7 +9,7 @@ use Kahlan\Reporter\Coverage\Exporter\Coveralls;
 $commandLine = $this->commandLine();
 $commandLine->option('src', 'default','src');
 $commandLine->option('spec', 'default', 'tests');
+$commandLine->option('reporter', 'default', 'tree');
 $commandLine->option('ff', 'default', 1);
-$commandLine->option('coverage', 'default', 3);
 //$commandLine->option('coverage-scrutinizer', 'default', 'scrutinizer.xml');
 //$commandLine->option('coverage-coveralls', 'default', 'coveralls.json');
---
+++ tests/unit/Adder.spec.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use PromptClasses\Adder;
+
+describe(Adder::class, function () {
+    describe('::add', function () {
+        it('adds integers', function () {
+            expect(Adder::add(1, 2))->toBe(3);
+        });
+    });
+});
+
+// `'dot'`, `'bar'`, `'json'`, `'tap'`, `'tree'` & `'verbose'` (default: `'dot'`).

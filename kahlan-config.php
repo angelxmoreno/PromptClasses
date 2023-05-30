b5722841e802cbb3de39ccd3590e21ba17bd2c7d
Angel S. Moreno <angelxmoreno@gmail.com>
2023-05-30 01:36:00 -0400

    created Fixture helper

--- kahlan-config.php
+++ kahlan-config.php
@@ -2,14 +2,15 @@
 /**
  * @var Kahlan\Cli\Kahlan $this
  */
-use Kahlan\Filter\Filters;
-use Kahlan\Reporter\Coverage\Exporter\Coveralls;
 
 
 $commandLine = $this->commandLine();
-$commandLine->option('src', 'default','src');
+$commandLine->option('src', 'default', 'src');
 $commandLine->option('spec', 'default', 'tests');
 $commandLine->option('reporter', 'default', 'tree');
 $commandLine->option('ff', 'default', 1);
 //$commandLine->option('coverage-scrutinizer', 'default', 'scrutinizer.xml');
 //$commandLine->option('coverage-coveralls', 'default', 'coveralls.json');
+
+const DS = DIRECTORY_SEPARATOR;
+const FIXTURES_DIR = __DIR__ . DS . 'tests' . DS . 'fixtures' . DS;

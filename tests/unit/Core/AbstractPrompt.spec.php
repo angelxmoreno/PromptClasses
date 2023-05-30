46177a7360c6bc29521f39935f270ebbea95b63e
Angel S. Moreno <angelxmoreno@gmail.com>
2023-05-30 01:37:21 -0400

    created AbstractPrompt tests

---
+++ tests/unit/Core/AbstractPrompt.spec.php
@@ -0,0 +1,37 @@
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+use PromptClasses\Core\AbstractPrompt;
+use PromptClasses\Test\Helpers\Fixture;
+use PromptClasses\Test\Helpers\HelloPrompt;
+
+describe(AbstractPrompt::class, function () {
+    describe('->send', function () {
+        it('returns null on failure value', function () {
+            $prompter = new HelloPrompt('');
+
+            allow($prompter)
+                ->toReceive('sendToOpenAI')
+                ->andRun(function () {
+                    throw new Exception();
+                });
+
+            $response = $prompter->send('Bob', 'Evening');
+            expect($response)->toBeNull();
+        });
+
+        it('returns expected value', function () {
+            $prompter = new HelloPrompt('');
+
+            allow($prompter)
+                ->toReceive('callOpenApi')
+                ->andReturn(Fixture::load('openaiResponse'));
+
+            $response = $prompter->send('Bob', 'Evening');
+            expect($response)->toBe('Hello Bob. Good Evening');
+        });
+    });
+});
